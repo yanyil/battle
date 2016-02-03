f1d0789e6cb2053246b5dc27fa5784b68cc13257
Yan-Yi Li <liyanyi@gmail.com>
2016-02-03 11:38:26 +0000

    added entering players

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ class Battle < Sinatra::Base
     erb :index
   end
 
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
